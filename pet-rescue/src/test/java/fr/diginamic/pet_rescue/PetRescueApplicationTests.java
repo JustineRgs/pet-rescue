5232983a8180de7c5b5d4291267b9e97668e63d2
Justine RAGUES <justineragues@gmail.com>
2024-10-15 15:51:35 +0200

    first commit

---
+++ pet-rescue/src/test/java/fr/diginamic/pet_rescue/PetRescueApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.pet_rescue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetRescueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
